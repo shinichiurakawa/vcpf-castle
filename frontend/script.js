cdcc7e46a863b143f5b229cfad3dc5d2c326ae86
1206797 <johncyruspalanca.pishva@g.softbank.co.jp>
2017-12-10 03:57:03 +0900

    update flash with background

--- frontend/script.js
+++ frontend/script.js
@@ -14,12 +14,45 @@ var rand = function (min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
+var wait_one_sec = function(){
+  return new Promise(function(resolve, reject){
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+}
+
 var main = function () {
   console.log("main");
-  setInterval(() => {
-    setCastle(rand(1, 3), rand(1, 3), rand(1, 3));
-  }, 1000);
+  // setInterval(() => {
+  //   setCastle(rand(1, 3), rand(1, 3), rand(1, 3));
+  // }, 1000);
   setCastle(1, 1, 1);
+  wait_one_sec()
+    .then(function () {
+      setCastle(1, 1, 2);
+    })
+    .then(wait_one_sec)
+    .then(function () {
+      setCastle(1, 2, 1);
+    })
+    .then(wait_one_sec)
+    .then(function () {
+      setCastle(1, 2, 2);
+    })
+    .then(wait_one_sec)
+    .then(function () {
+      setCastle(2, 2, 2);
+    })
+    .then(wait_one_sec)
+    .then(function () {
+      setCastle(2, 3, 2);
+    })
+    .then(wait_one_sec)
+    .then(function () {
+      setCastle(3, 3, 3);
+    })
+    .then(wait_one_sec)
 };
 
 $(function () {
