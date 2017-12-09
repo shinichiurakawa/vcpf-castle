305e5e99f611123f8cceb6ee662b1718075937c5
1206797 <johncyruspalanca.pishva@g.softbank.co.jp>
2017-12-10 02:54:27 +0900

    update image and test flash

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,29 @@
-var reload_me = function(){
+var reload_me = function () {
   setTimeout(() => {
     window.location.reload();
   }, 10000);
-}
+};
 
-var main = function(){
+var setCastle = function (castle, wall, garden) {
+  $(".castle").css("background-image", 'url("images/1_' + castle + '.png")');
+  $(".wall").css("background-image", 'url("images/2_' + wall + '.png")');
+  $(".garden").css("background-image", 'url("images/3_' + garden + '.png")');
+};
+
+var rand = function (min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+
+var main = function () {
   console.log("main");
-  $(".sky").css("background-image", 'url("images/1_1.png")');
-  $(".castle").css("background-image", 'url("images/2_1.png")');
-  $(".wall").css("background-image", 'url("images/3_1.png")');
-  $(".garden").css("background-image", 'url("images/4_1.png")');
-  // debugger;
-}
+  setInterval(() => {
+    setCastle(rand(1, 3), rand(1, 3), rand(1, 3));
+  }, 1000);
+  setCastle(1, 1, 1);
+};
 
 $(function () {
   main();
 
   reload_me();
-})
+});
